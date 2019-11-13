fad108004c008c39be6ee8359e1761d35b30462e
kaarthikraja <kaarthik.raja@freshworks.com>
2019-11-13 18:05:58 +0530

    Bug correction

--- Data Structures & Algorithms/src/arrays/MaximumContiguousSubArray.java
+++ Data Structures & Algorithms/src/arrays/MaximumContiguousSubArray.java
@@ -7,21 +7,21 @@ public class MaximumContiguousSubArray {
 		// Solve the maximum contiguous sub array using kadane's algorithm
 		// Here you have to find the maximum sum in any given sub array
 		
-		int[] array = {1, 3, -4, -1, 2, -1, 7};
+		int[] array = {30, 3, -4, -1, 2, -1, 1};
 		
-		int value  = kanadesAlgorithm(array);
+		int value  = kadanesAlgorithm(array);
 		
 		System.out.println("The maximum contiguous sub array is " + value);
 	}
 
-	private static int kanadesAlgorithm(int[] array) {
+	private static int kadanesAlgorithm(int[] array) {
 		// TODO Auto-generated method stub
 		int a = Integer.MIN_VALUE;
 		int b = 0;
 		
 		for (int i=0; i<array.length; i++) {
 			
-			a = a + array[i];
+			b = b + array[i];
 			
 			if(a<b) {
 				a=b;
